8a24a324a34e656629a4aa3e6a1253fadb2e2431
WeiWei <yher25@gmail.com>
2022-08-16 10:52:45 +0800

    Update Struct.sol

--- complete/Struct.sol
+++ complete/Struct.sol
@@ -7,7 +7,31 @@ contract Struct {
         string name;
         int price;
         int amount;
-        address seller;
+        address seller; //msg.sender
+    }
+
+    Food[] public foods; //array
+
+    function createFood() public {
+        //First
+        Food memory apple = Food("apple",50,1,msg.sender);//memory storage
+        foods.push(apple);
+        
+        //Second
+        Food memory soup = Food({
+            name : "soup",
+            amount : 1,
+            price : 50,
+            seller : msg.sender
+        });
+        foods.push(soup);
+        
+        //Third
+        foods.push(Food("apple",50,1,msg.sender));
+    }
+
+    function getFoods() public view returns(Food[] memory){
+        return foods;
     }
 }
 
