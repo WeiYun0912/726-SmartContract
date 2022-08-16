ee4ea2cf39c3b61d361749661b44dde2b2ddcfad
WeiWei <yher25@gmail.com>
2022-08-16 10:34:08 +0800

    Update GlobalVariables.sol

--- complete/GlobalVariables.sol
+++ complete/GlobalVariables.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.8.7;
 // Global Variables
 
 //msg.sender, block.timestamp, block.number, msg.balance, msg.value
+//address
+
+//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+//Gwei
 
 contract GlobalVariables {
     address public sender;
@@ -9,13 +13,18 @@ contract GlobalVariables {
     uint public blockNumber;
     uint public balance;
     uint public value;
+    uint public contractBalance;
 
-    function setGlobalVariables() public payable {
+    function setGlobalVariables() public payable{
         sender = msg.sender;
         blockTime = block.timestamp;
         blockNumber = block.number;
         balance = msg.sender.balance;
         value = msg.value;
+    }
+
+    function getBalance() public payable{
+        contractBalance = address(this).balance; // address(this) -> 智能合約的address(地址)
     }
 }
 
