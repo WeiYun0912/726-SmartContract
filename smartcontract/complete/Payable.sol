905cd68ab81379fc4f7eff64a7bc431879bea764
WeiWei <yher25@gmail.com>
2022-08-30 11:26:35 +0800

    Create Payable.sol

---
+++ smartcontract/complete/Payable.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Payable {
+    address payable public owner;
+    mapping(address => uint256) balances;
+    event Log(address sender,uint256 value);
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit() public payable {
+        require(msg.value >= 1 ether, "At least 1 ether.");
+        balances[msg.sender] += msg.value;
+        emit Log(msg.sender,msg.value);
+    }
+
+    function sendEther(address _receiver) public payable {
+        (bool sent, ) = _receiver.call{value: msg.value}("");
+        require(sent, "Fail to send ether.");
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
